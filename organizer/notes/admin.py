8df0cb299d11805a3e47a5a4a78e3fc46677f932
amir <amirtaghavi20262gmail.com>
2026-07-24 08:17:11 +0330

    chore: set up admin panel , main urls and routes

--- organizer/notes/admin.py
+++ organizer/notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User , Course , Note
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+admin.site.register(User , UserAdmin)
+admin.site.register(Course)
+admin.site.register(Note)
+
