aedfbe7b32fc13d353130d807b4686cab9abe681
amir <amirtaghavi20262gmail.com>
2026-07-25 01:11:54 +0330

    feat: create forms.py and add signup form and signup page

--- organizer/notes/urls.py
+++ organizer/notes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import *
 
 urlpatterns =[
-    
+    path("signup/" , signup ,name = "signup"),
+    path('' , course_list , name= "course_list")
 ]
--- organizer/notes/views.py
+++ organizer/notes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render , get_object_or_404 , redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request , user)
+            return redirect("course_list")
+
+    else:
+        form = SignUpForm()
+    return render(request , "registration/signup.html" , {"form" : form})
+
+
+@login_required
+def course_list(request):
+    pass
